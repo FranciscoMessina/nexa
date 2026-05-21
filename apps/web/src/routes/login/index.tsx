4495619f748239b322478e1e11fc7a3e4373635f
Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2026-05-20 21:36:06 -0300

    feat: wire auth state into app routes

---
+++ apps/web/src/routes/login/index.tsx
@@ -0,0 +1,10 @@
+import { createFileRoute } from "@tanstack/react-router"
+import { LoginPage } from "@/features/auth"
+
+export const Route = createFileRoute("/login/")({
+  component: LoginRoute,
+})
+
+function LoginRoute() {
+  return <LoginPage />
+}
