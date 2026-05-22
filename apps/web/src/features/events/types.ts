a2983fbe8d497324a9b2692bb05b775a3cafcf90
Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2026-05-22 00:06:36 -0300

    feat: add event label component

---
+++ apps/web/src/features/events/types.ts
@@ -0,0 +1,20 @@
+export type EventLabelData = {
+  type: string
+  text: string
+}
+
+export type EventImage = {
+  src: string
+  alt: string
+}
+
+export type EventCardData = {
+  label: EventLabelData
+  title: string
+  location: string
+  date: Date | string
+  category: string
+  image: EventImage
+  ctaText: string
+  ctaHref?: string
+}
